2839732abd2437ca7345eb454778d60068c73f36
AndyFerrie <93442058+AndyFerrie@users.noreply.github.com>
2023-11-03 08:19:35 +0000

    Adds imports to app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
-import TopBar from "./scenes/global/TopBar";
-import { ColorModeContext, useMode } from "./theme";
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Topbar from "./scenes/global/TopBar";
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard";
+import Team from "./scenes/team";
+import Invoices from "./scenes/invoices";
+import Contacts from "./scenes/contacts";
+import Bar from "./scenes/bar";
+import Form from "./scenes/form";
+import Line from "./scenes/line";
+import Pie from "./scenes/pie";
+import FAQ from "./scenes/faq";
+import Geography from "./scenes/geography";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+import Calendar from "./scenes/calendar/calendar";
 
 function App() {
     const [theme, colorMode] = useMode();
@@ -10,7 +24,7 @@ function App() {
                 <CssBaseline />
                 <div className="app">
                     <main className="content">
-                        <TopBar />
+                        <Topbar />
                     </main>
                 </div>
             </ThemeProvider>
