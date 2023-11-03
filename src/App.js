201f1e54f29bba75dabb0e5cd0f296aa866db856
AndyFerrie <93442058+AndyFerrie@users.noreply.github.com>
2023-11-03 07:45:23 +0000

    Adds theme provider and color mode context to app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import TopBar from "./scenes/global/TopBar";
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+
 function App() {
-    return <div className="app"></div>;
+    const [theme, colorMode] = useMode();
+    return (
+        <ColorModeContext.Provider value={colorMode}>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <div className="app">
+                    <main className="content">
+                        <TopBar />
+                    </main>
+                </div>
+            </ThemeProvider>
+        </ColorModeContext.Provider>
+    );
 }
 
 export default App;
